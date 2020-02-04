b72ef15959238de0c959ce67386d5cb8589a72fc
Steve Wall <syntakks@gmail.com>
2020-02-03 23:39:42 -0600

    3/4 sections complete
    Finishing up UI polish. Label text still needs formatting.

---
+++ VendingMachine/VendingMachine.swift
@@ -0,0 +1,147 @@
+//
+//  VendingMachine.swift
+//  VendingMachine
+//
+//  Created by Steve Wall on 2/3/20.
+//  Copyright © 2020 Treehouse Island, Inc. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum VendingSelection: String {
+    case soda
+    case dietSoda
+    case chips
+    case cookie
+    case sandwich
+    case wrap
+    case candyBar
+    case popTart
+    case water
+    case fruitJuice
+    case sportsDrink
+    case gum
+    
+    func icon() -> UIImage {
+        if let image = UIImage(named: self.rawValue) {
+            return image
+        } else {
+            return #imageLiteral(resourceName: "default")
+        }
+    }
+}
+
+protocol VendingItem {
+    var price: Double { get }
+    var quantity: Int { get set }
+}
+
+protocol VendingMachine {
+    var selection: [VendingSelection] { get }
+    var inventory: [VendingSelection: VendingItem] { get set }
+    var amountDeposited: Double { get set }
+     
+    init(inventory: [VendingSelection: VendingItem])
+    func vend(selection: VendingSelection, quantity: Int) throws
+    func deposit(_ aount: Double)
+    func item(forSelection selection: VendingSelection) -> VendingItem?
+}
+
+struct Item: VendingItem {
+    let price: Double
+    var quantity: Int
+}
+
+enum InventoryError: Error {
+    case invalidResource
+    case conversionFailure
+    case invalidSelection
+}
+
+class PlistConverter {
+    static func dictionary(fromFile name: String, ofType type: String) throws -> [String: AnyObject] {
+        // First get path of resource.
+        guard let path = Bundle.main.path(forResource: name, ofType: type) else {
+            throw InventoryError.invalidResource
+        }
+        //print(path)
+        // Gets dictionary from file. Need to type cast.
+        guard let dictionary = NSDictionary(contentsOfFile: path) as? [String : AnyObject] else {
+            throw InventoryError.conversionFailure
+        }
+        //print (dictionary)
+        return dictionary
+    }
+}
+
+class InventoryUnarchiver {
+    static func vendingInventory(fromDictionary: [String : AnyObject]) throws -> [VendingSelection : VendingItem]{
+        var inventory: [VendingSelection : VendingItem] = [:]
+        for (key, value) in fromDictionary {
+            
+            if let itemDictionary = value as? [String : Any],
+                let price = itemDictionary["price"] as? Double,
+                let quantity = itemDictionary["quantity"] as? Int {
+                let item = Item(price: price, quantity: quantity)
+                
+                guard let selection = VendingSelection(rawValue: key) else {
+                    throw InventoryError.invalidSelection
+                }
+                inventory.updateValue(item, forKey: selection)
+            } else {
+                print("InventoryUnarchiver: Could not get a valid [VendingSelection : VendingItem]")
+            }
+        }
+        return inventory
+    }
+}
+
+enum VendingMachineError: Error {
+    case invalidSelection
+    case outOfStock
+    case insufficientFunds(required: Double)
+}
+
+class FoodVendingMachine: VendingMachine {
+    let selection: [VendingSelection] = [
+        .soda, .dietSoda, .chips, .cookie, .sandwich, .wrap, .candyBar, . popTart, .water, .fruitJuice, .sportsDrink, .gum
+    ]
+    var inventory: [VendingSelection : VendingItem]
+    var amountDeposited: Double = 10.0
+    
+    required init(inventory: [VendingSelection : VendingItem]) {
+        self.inventory = inventory
+    }
+    
+    func vend(selection: VendingSelection, quantity: Int) throws {
+        // Validate selection
+        guard var item = inventory[selection] else {
+            throw VendingMachineError.invalidSelection
+        }
+        // Validate Available quantity
+        guard item.quantity >= quantity else {
+            throw VendingMachineError.outOfStock
+        }
+        // Validate funds
+        let totalPrice = item.price * Double(quantity)
+        if amountDeposited >= totalPrice {
+            // Vend Item
+            amountDeposited -= totalPrice
+            item.quantity -= quantity
+            inventory.updateValue(item, forKey: selection)
+        } else {
+            // Insufficient Funds
+            let amountRequired = totalPrice - amountDeposited
+            throw VendingMachineError.insufficientFunds(required: amountRequired)
+        }
+    }
+    
+    func deposit(_ aount: Double) {
+        
+    }
+    
+    func item(forSelection selection: VendingSelection) -> VendingItem? {
+        return inventory[selection]
+    }
+}
---
+++ VendingMachine/ViewController.swift
@@ -0,0 +1,171 @@
+//
+//  ViewController.swift
+//  VendingMachine
+//
+//  Created by Pasan Premaratne on 12/1/16.
+//  Copyright © 2016 Treehouse Island, Inc. All rights reserved.
+//
+
+import UIKit
+
+fileprivate let reuseIdentifier = "vendingItem"
+fileprivate let screenWidth = UIScreen.main.bounds.width
+
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var balanceLabel: UILabel!
+    @IBOutlet weak var quantityLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var quantityStepper: UIStepper!
+    
+    let vendingMachine: VendingMachine
+    var currentSelection: VendingSelection?
+    
+    required init?(coder: NSCoder) {
+        do {
+            let dictionary = try PlistConverter.dictionary(fromFile: "VendingInventory", ofType: "plist")
+            let newInventory = try InventoryUnarchiver.vendingInventory(fromDictionary: dictionary)
+            self.vendingMachine = FoodVendingMachine(inventory: newInventory)
+        } catch let error {
+            fatalError("\(error)")
+        }
+        super.init(coder: coder)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        setupCollectionViewCells()
+        updateDisplayWith(balance: vendingMachine.amountDeposited, totalPrice: 0.0, itemPrice: 0.0, itemQuantity: 1)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Setup
+
+    func setupCollectionViewCells() {
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 20, right: 0)
+        
+        let padding: CGFloat = 10
+        let itemWidth = screenWidth/3 - padding
+        let itemHeight = screenWidth/3 - padding
+        
+        layout.itemSize = CGSize(width: itemWidth, height: itemHeight)
+        layout.minimumLineSpacing = 10
+        layout.minimumInteritemSpacing = 10
+        
+        collectionView.collectionViewLayout = layout
+    }
+    // MARK: - Vending Machine
+    
+    @IBAction func purchase() {
+        if let currentSelection = currentSelection {
+            do {
+                try vendingMachine.vend(selection: currentSelection, quantity: Int(quantityStepper.value))
+                updateDisplayWith(balance: vendingMachine.amountDeposited, totalPrice: 0.0, itemPrice: 0.0, itemQuantity: 1)
+            } catch {
+                // FIXME: Error handling code
+            }
+            // Reset the cell selection state.
+            if let indexPath = collectionView.indexPathsForSelectedItems?.first {
+                collectionView.deselectItem(at: indexPath, animated: true)
+                updateCell(having: indexPath, selected: false)
+            }
+        } else {
+            // FIXME: Alert user to no selection
+        }
+    }
+    
+    func updateDisplayWith(balance: Double? = nil, totalPrice: Double? = nil, itemPrice: Double? = nil, itemQuantity: Int? = nil ) {
+        if let balanceValue = balance {
+            balanceLabel.text = "\(balanceValue)"
+        }
+        
+        if let totalValue = totalPrice {
+            totalLabel.text = "\(totalValue)"
+        }
+        
+        if let priceValue = itemPrice {
+            priceLabel.text = "\(priceValue)"
+        }
+        
+        if let quantityValue = itemQuantity {
+            quantityLabel.text = "\(quantityValue)"
+        }
+    }
+    
+    func updateTotalPrice(for item: VendingItem) {
+        let totalPrice = item.price * quantityStepper.value
+        updateDisplayWith(totalPrice: totalPrice)
+    }
+    
+    @IBAction func updateQuantity(_ sender: UIStepper) {
+        let quantity = Int(quantityStepper.value)
+        updateDisplayWith(itemQuantity: quantity)
+        
+        if let currentSelection = currentSelection,
+            let item = vendingMachine.item(forSelection: currentSelection) {
+            updateTotalPrice(for: item)
+        }
+    }
+    
+    // MARK: UICollectionViewDataSource
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return vendingMachine.selection.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? VendingItemCell else { fatalError() }
+        let item = vendingMachine.selection[indexPath.row]
+        cell.iconView.image = item.icon()
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        updateCell(having: indexPath, selected: true)
+        
+        quantityStepper.value = 1
+        updateDisplayWith(totalPrice: 0.0, itemQuantity: 1)
+        
+        currentSelection = vendingMachine.selection[indexPath.row]
+        if let currentSelection = currentSelection,
+            let item = vendingMachine.item(forSelection: currentSelection) {
+            let totalValue = item.price * Double(quantityStepper.value)
+            updateDisplayWith(totalPrice: totalValue, itemPrice: item.price)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        updateCell(having: indexPath, selected: false)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didHighlightItemAt indexPath: IndexPath) {
+        updateCell(having: indexPath, selected: true)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didUnhighlightItemAt indexPath: IndexPath) {
+        updateCell(having: indexPath, selected: false)
+    }
+    
+    func updateCell(having indexPath: IndexPath, selected: Bool) {
+        
+        let selectedBackgroundColor = UIColor(red: 41/255.0, green: 211/255.0, blue: 241/255.0, alpha: 1.0)
+        let defaultBackgroundColor = UIColor(red: 27/255.0, green: 32/255.0, blue: 36/255.0, alpha: 1.0)
+        
+        if let cell = collectionView.cellForItem(at: indexPath) {
+            cell.contentView.backgroundColor = selected ? selectedBackgroundColor : defaultBackgroundColor
+        }
+    }
+    
+    
+}
+
